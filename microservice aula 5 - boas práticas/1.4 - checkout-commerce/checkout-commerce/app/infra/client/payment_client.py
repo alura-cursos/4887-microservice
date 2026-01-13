6ac433071cc8e8c31e56922a721d4e75a302b9b7
Rafael Bomfim <58951965+rafaelbomfimjr@users.noreply.github.com>
2026-01-13 18:14:32 -0300

    Add files via upload

---
+++ microservice aula 5 - boas práticas/1.4 - checkout-commerce/checkout-commerce/app/infra/client/payment_client.py
@@ -0,0 +1,40 @@
+import httpx
+
+from app.checkout.checkout_request import PaymentMethodRequest
+from app.client_manager import client_manager
+
+
+class PaymentClient:
+    def __init__(self, client: httpx.AsyncClient):
+        self.client = client
+
+    async def process(
+        self,
+        total_amount: float,
+        payment_method: PaymentMethodRequest,
+        customer_email: str,
+    ):
+        try:
+            payload = {
+                "amount": total_amount,
+                "payment_method": {
+                    "type": payment_method.type,
+                    "card_number": payment_method.card_number,
+                    "card_expiry": payment_method.card_expiry,
+                    "card_cvv": payment_method.card_cvv,
+                },
+                "customer_email": customer_email,
+            }
+            response = await self.client.post("/payments/process", json=payload)
+            response.raise_for_status()
+            transaction_id = response.json()["transactionId"]
+            return {"transaction_id": transaction_id, "error": None}
+        except httpx.HTTPStatusError as e:
+            error_response = e.response.text
+            return {"transaction_id": None, "error": error_response}
+        except Exception as e:
+            return {"transaction_id": None, "error": str(e)}
+
+
+def get_payment_client() -> PaymentClient:
+    return PaymentClient(client=client_manager.payment_client)
